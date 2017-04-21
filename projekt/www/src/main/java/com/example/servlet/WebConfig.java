edb1410a49edfe8bebecd0522cabb14be3148c71
kubamarchwicki <kuba@marchwicki.pl>
2017-04-21 15:41:55 +0200

    Jackson mappings

--- projekt/www/src/main/java/com/example/servlet/WebConfig.java
+++ projekt/www/src/main/java/com/example/servlet/WebConfig.java
@@ -1,18 +1,35 @@
 package com.example.servlet;
 
+import com.example.dictionary.translation.DictionaryWord;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.addMixIn(DictionaryWord.class, DictionaryWordMixIn.class);
+
+        MappingJackson2HttpMessageConverter converter =
+                new MappingJackson2HttpMessageConverter(mapper);
+        converters.add(converter);
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
@@ -23,4 +40,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.viewResolver(viewResolver);
     }
 
+    public static class DictionaryWordMixIn {
+
+        public DictionaryWordMixIn(@JsonProperty("polishWord") String polishWord,
+                                   @JsonProperty("englishWord") String englishWord) {
+        }
+
+    }
 }
--- projekt/www/src/main/java/com/example/servlet/webapp/MyController.java
+++ projekt/www/src/main/java/com/example/servlet/webapp/MyController.java
@@ -2,11 +2,20 @@ package com.example.servlet.webapp;
 
 import com.example.dictionary.translation.DictionaryWord;
 import com.example.dictionary.translation.TranslationService;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -19,7 +28,7 @@ public class MyController {
         this.service = service;
     }
 
-    @GetMapping("/translation.html")
+    @GetMapping("/translations.html")
     public ModelAndView translate(@RequestParam("word") String word) {
 
         ModelAndView mv = new ModelAndView();
@@ -30,12 +39,19 @@ public class MyController {
         return mv;
     }
 
-    @PostMapping("/translation/{word}")
+    @GetMapping(value = "/translations/{word}",
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    public ResponseEntity doTranslations(@PathVariable("word") String word) {
+    public ResponseEntity<List<DictionaryWord>> doTranslations(@PathVariable("word") String word) {
         List<DictionaryWord> words = service.getTranslationsForWord(word);
+
+        return ResponseEntity.ok(words);
+    }
+
+    @PostMapping(value = "/translations", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity saveWord(@RequestBody DictionaryWord word) {
+        System.out.println("str = " + word);
 
         return ResponseEntity.noContent().build();
     }
-
 }
